86d0407cde1632b3d984b7ab16a6267d2ac82b61
BrendonH <hall.brendonp@gmail.com>
2023-11-14 16:25:36 -0600

    Add Phone Number to CustomUser

--- UserManagement/admin.py
+++ UserManagement/admin.py
@@ -1,14 +1,20 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django import forms
+from django.utils.translation import gettext_lazy as _
 from .models import CustomUser
+from phonenumber_field.formfields import PhoneNumberField
+
+class PhoneForm(forms.Form):
+    number = PhoneNumberField(region="US")
 
 @admin.register(CustomUser)
 class CustomUserAdmin(UserAdmin):
        
     list_display = (
-        'name', 
-        "is_staff", 
-        "is_active",
+        'name',
+        'is_staff', 
+        'is_active',
         )
     search_fields = (
         'email',
@@ -16,6 +22,31 @@ class CustomUserAdmin(UserAdmin):
     ordering = (
         "email",
         )
+    fieldsets = (
+        (None, {
+            "fields": (
+                'username',
+            ),
+        }),
+        (_('Personal Information'), {
+            'fields': (
+                'first_name',
+                'last_name',
+                'email',
+                'phone_number'
+            ),
+        }),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        })
+    )
+    
     add_fieldsets = (
             (
                 None,
@@ -26,6 +57,7 @@ class CustomUserAdmin(UserAdmin):
                     'fields': (
                         'first_name',
                         'last_name',
+                        'phone_number',
                         'email', 
                         'username', 
                         'password1', 
--- UserManagement/models.py
+++ UserManagement/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 class CustomUser(AbstractUser):
     email = models.EmailField(unique=True, blank=False)
     first_name = models.CharField(max_length=255, blank=False)
     last_name = models.CharField(max_length=255, blank=False)
+    phone_number = PhoneNumberField(region="US", blank=False, null=False, unique=True)
     def __str__(self) -> str:
         return self.get_full_name()
