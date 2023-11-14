384705f460ae7c40e341d1bd32312e702ca759bd
BrendonH <hall.brendonp@gmail.com>
2023-11-13 21:45:03 -0600

    Add CustomUser - Fields, Tests, Admin

---
+++ UserManagement/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+# from .forms import CustomUserChangeForm, CustomUserCreationForm
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # add_form = CustomUserCreationForm
+    # form = CustomUserChangeForm
+    # model = CustomUser
+    list_display = ("email", "is_staff", "is_active",)
+    search_fields = ("email",)
+    ordering = ("email",)
+    add_fieldsets = (
+            (
+                None,
+                {
+                    'classes': ('wide',),
+                    'fields': ('email', 'username', 'password1', 'password2'),
+                },
+            ),
+        )
+    pass
+
+# admin.site.register(CustomUser, CustomUserAdmin)
