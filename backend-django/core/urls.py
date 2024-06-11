58175c7d2e3d2ee52a172dc2f09b1c7310538034
BrendonH <hall.brendonp@gmail.com>
2024-06-11 15:05:44 -0600

    RestAPI Basic Setup

--- backend-django/core/urls.py
+++ backend-django/core/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from Appointments.admin import custom_admin_site
 
 from . import views
@@ -23,5 +23,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-    path('customadmin/', custom_admin_site.urls)
+    path('customadmin/', custom_admin_site.urls),
+    path('api-auth/', include('rest_framework.urls'))
 ]
