fc4c9e7f62a7178bd375786107d5cd0b9742806d
BrendonH <hall.brendonp@gmail.com>
2024-07-25 14:45:43 -0600

    Adjust JS Reference, Add OnChange Functions

--- backend-django/core/static/availability/update_hourly_availability_given_day_and_user_classes.js
+++ backend-django/core/static/availability/update_hourly_availability_given_day_and_user_classes.js
@@ -64,7 +64,13 @@ class AvailabilityManager {
                 }
                 select.append(option);
             });
-            // select.on('change', handleDateChange);
+            select.on('change', async (e) => {
+                await this.getCalendarData()
+                .then(response => {
+                        this.buildCalendarTable(response)
+                    })
+                    .catch(err => console.log(err))
+            });
             return select;
         };
 
@@ -219,7 +225,7 @@ class AvailabilityManager {
     };
     async buildUI() { // Depends on setupDOM, buildMonthYearSelector, buildSlotTable, buildCalendarTable
         this.buildMonthYearSelector()
-        await this.getCalendar()
+        await this.getCalendarData()
         .then(response => {
                 this.buildCalendarTable(response)
             })
@@ -271,7 +277,7 @@ class AvailabilityManager {
             }
         }
     }
-    async getCalendar() { // Depends on getDate
+    async getCalendarData() { // Depends on getDate
         const date = this.getDate()
         const options = {
             year: 'numeric',
@@ -316,169 +322,6 @@ class AvailabilityManager {
             });
         });
     }
-    //delete
-    handleDateChange (ev) {
-        const previousButton = this.previousButton
-        // Function to retrieve availability data
-        handleDateButtonClick
-        getCalendar // ok
-        getSlotData // ok
-        postSlotData // ok
-        buildMonthYearSelector // ok
-        buildSlotTable // ok
-        buildCalendarTable // ok
-
-        // Function to build calendar table
-        const buildCalendarTable = (data) => {
-            console.log(data)
-            const date = getDate()
-            // Clear the contents of the HTML container
-            $('#Availability-Date').empty();
-            $('#Availability-Time').empty();
-            const days_of_week = data.days_of_week
-            const weeks = data.weeks
-
-            const formatDate = (dateString) => {
-                const date = new Date(dateString);
-                const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"];
-                const month = months[date.getUTCMonth()];
-                const day = String(date.getUTCDate()).padStart(2, '0');
-                return `${month} ${day}`;
-            }
-            // Create the table structure
-            const table = $('<table>', {class: "table table-striped table-hover"}).appendTo( $('#Availability-Date') );
-            const thead = $('<thead>' , {}).appendTo(table);
-            const tbody = $('<tbody>', {}).appendTo(table);
-
-            // Create the first header row for dates
-            const headerRow1 = $('<tr>', {}).appendTo(thead);
-
-            days_of_week.forEach(day => {
-                $('<th>', {
-                    class: 'text-center align-middle',
-                    scope: 'col',
-                    text: day
-                }).appendTo(headerRow1);
-            });
-            async function handleDateButtonClick(event) {
-
-                // const formatTimeToAMPM = timeString => {
-                //     const [hours, minutes] = timeString.split(':');
-                //     const period = hours >= 12 ? 'PM' : 'AM';
-                //     const adjustedHours = hours % 12 || 12;
-                //     return `${adjustedHours}:${minutes} ${period}`;
-                // };
-
-                const buildSlotTable = (slots) => {
-                    const createSlot = (item, index) => {
-                        // Functions
-                        const createTableRow = () => $('<tr>'); // Function to create a new table row
-                        const createSlotButton = (item) => { // Function to create a button for a slot
-                            return $('<button>', {
-                                type: 'button',
-                                class: 'btn btn-sm',
-                                text: formatTimeToAMPM(item.start_time)
-                            });
-                        };
-                        const createTableCell = (button) => { // Function to create a table cell with the provided button
-                            return $('<td>', {
-                                class: 'text-center align-middle'
-                            }).append(button);
-                        };
-                        const handleButtonClick = (button) => { // Function to handle button click event
-                            console.log(button.data());
-                            updateSlotData(button.data('id'))
-                                .then(response => {
-                                    this.formatColors(button, {table: "slot", label: response.status}) // Update the button color based on the slot status
-                                })
-                                .catch(err => console.log(err));
-                        };
-                        // Execution
-                        const tableBody = $('#Availability-Time-Table-Body') // created below
-                        if (index % 2 === 0) { // Add a new row to the table body every two items
-                            createTableRow().appendTo(tableBody);
-                        }
-                        const row = tableBody.children().last(); // Get the last row added to tbody
-                        const button = createSlotButton(item) // Create a button for the slot
-                            .data(item) // Attach the item data to the button
-                            .on('click', function() {
-                                handleButtonClick($(this)); // Handle button click event
-                            });
-                        this.formatColors(button, {table: "slot", label: item.status}) // Update the button color based on the slot status
-                        // handleSlotColorUpdate(button, item.status);
-                        createTableCell(button).appendTo(row); // Create a table cell with the button and append it to the row
-                    }
-                    const timeContainer = $('#Availability-Time')
-                    const dateStr = currentButton.data('date');
-                    // Create the table structure
-                    const table = $('<table>', { class: 'table table-striped table-hover' }).appendTo(timeContainer);
-                    const thead = $('<thead>', { }).appendTo(table);
-                    const tbody = $('<tbody>', { id: 'Availability-Time-Table-Body' }).appendTo(table);
-                    // Create the first header row for dates
-                    const headerRow = $('<tr>', { }).appendTo(thead);
-                    $('<th>', {  // Add Slot Header1
-                        class: 'text-center align-middle',
-                        scope: 'col',
-                        text: dateStr
-                    }).appendTo(headerRow)
-                    $('<th>', {  // Add Slot Header2
-                        class: 'text-center align-middle',
-                        scope: 'col',
-                        text: "123"
-                    }).appendTo(headerRow)
-                    slots.forEach((item, index) => { // Iterate over each slot item
-                        createSlot(item, index)
-                    });
-                }
-
-                const currentButton = $(event.target)
-                $('#Availability-Time').empty()
-                // handleDateColorUpdate()
-
-                // const slots = currentButton.data('slots');
-                const dateObj = currentButton.data('date_obj');
-                await retrieveSlotData(dateObj.id)
-                .then(response => {
-                    console.log("Response: ", response)
-                    buildSlotTable(response.slots)
-                })
-                .catch(err => console.log(err))
-            }
-
-
-            // Iterate over weeks and days to fill the table
-            weeks.forEach(week => {
-                const weekRow = $('<tr></tr>');
-                weekRow.appendTo(tbody)
-                week.forEach(day => {
-                    const dayCell = $('<td class="text-center align-middle"></td>');
-                    const button = $('<button type="button" class="btn btn-sm"></button>')
-                    dayCell.append(button).appendTo(weekRow)
-                    if (day.date_obj !== null) {
-                        const dateObj = day.date_obj;
-                        const dateStr = formatDate(dateObj.date)
-                        button.text(dateStr)
-                        button.data({ slots: !!dateObj.slots, date: dateStr.toString(), date_obj: dateObj})
-                        button.addClass('btn-primary')
-                        button.on('click', handleDateButtonClick);
-                    } else {
-                        button.text(`---`)
-                        button.data({ slots: null })
-                        button.addClass('btn-outline-secondary')
-                        // button.disabled = true // not working
-                    }
-                })
-            })
-        };
-
-
-        retrieveAvailabilityData(ev)
-            .then(data => {
-                buildCalendarTable('#gridContainer', data) // getCalendar
-            })
-            .catch(err => console.error('Error fetching data:', err));
-    };
-
     init(){
         $(document).ready(() => {
             this.setupAjax();
